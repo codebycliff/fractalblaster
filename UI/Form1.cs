c07dece96ccfc203d0f3b3e4114fa4a4feda93c9
balexander <balexander@ab504855-4d6b-4199-a8d8-a2e244a42e5e>
2011-02-03 18:53:10 +0000

    Added UI Application to Solution

---
+++ UI/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Engine;
+
+namespace UI
+{
+    public partial class Form1 : Form
+    {
+        Engine.Engine prog;
+
+        public Form1()
+        {
+            InitializeComponent();
+            prog = new Engine.Engine();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                prog.openFile(openFileDialog1.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            prog.playFile();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            Engine.Engine.Pause();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Engine.Engine.UnPause();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Engine.Engine.Stop();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
