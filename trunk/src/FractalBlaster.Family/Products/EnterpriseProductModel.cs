b9611cc8bec1228d05d1a492e71f57d6965cf5c7
cbraton <cbraton@ab504855-4d6b-4199-a8d8-a2e244a42e5e>
2011-02-14 14:19:00 +0000

    Attempting to check in the rest of the solution changes from the merge. This changeset includes the 'FractalBlaster.Family' projcect.

---
+++ trunk/src/FractalBlaster.Family/Products/EnterpriseProductModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Resources;
+using FractalBlaster.Core;
+using FractalBlaster.Runtime;
+
+namespace FractalBlaster.Products {
+    
+    public class EnterpriseProductModel : IProductModel {
+
+        public EnterpriseProductModel() {
+            mEngineTypes = new List<Type> () {
+                typeof(AudioEngine),
+                typeof(VideoEngine),
+                typeof(PhotoEngine)
+            };
+        }
+
+        public String Name {
+            get { return "Enterprise";  }
+        }
+
+        public  IEnumerable<Type> EngineTypes {
+            get { return mEngineTypes; } 
+        }
+
+        public IContainerControl DefaultUI {
+            get { return new ContainerControl(); }
+        }
+
+        public Boolean AllowsCustomEngines {
+            get { return true; }
+        }
+
+        public Boolean AddCustomEngine(Type engine) {
+            if (AllowsCustomEngines) {
+                mEngineTypes.Add(engine);
+                return true;
+            }
+            return false;
+        }
+
+        #region [ Private ]
+
+        private List<Type> mEngineTypes;
+        
+        #endregion
+
+    }
+
+}
---
+++ trunk/src/FractalBlaster.Family/Products/ExpressProductModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Resources;
+using FractalBlaster.Core;
+using FractalBlaster.Core.UI;
+using System.Collections.Generic;
+
+namespace FractalBlaster.Products {
+
+    public class ExpressProductModel : IProductModel {
+
+        public ExpressProductModel(IEngine engine) {
+            Engine = engine;
+        }
+
+        public String Name {
+            get { return "Express";  }
+        }
+
+        public IEnumerable<Type> EngineTypes {
+            get { return new Type[] { Engine.GetType() }; }
+        }
+
+        public IContainerControl DefaultUI {
+            get { return new ContainerControl(); }
+        }
+
+        public bool AllowsCustomEngines {
+            get { return false; }
+        }
+
+        public IEngine Engine { get; set; }
+
+    }
+
+}
