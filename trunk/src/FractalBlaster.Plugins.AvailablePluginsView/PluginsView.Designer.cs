20261d36c9c7c605b9d2c969f542384e58c5043b
balexander <balexander@ab504855-4d6b-4199-a8d8-a2e244a42e5e>
2011-02-22 23:26:55 +0000

    Changed PluginsView to not have border

--- trunk/src/FractalBlaster.Plugins.AvailablePluginsView/PluginsView.Designer.cs
+++ trunk/src/FractalBlaster.Plugins.AvailablePluginsView/PluginsView.Designer.cs
@@ -69,7 +69,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(257, 397);
             this.Controls.Add(this.mPluginTree);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "PluginsView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Available Plugins";
             this.ResumeLayout(false);
 
