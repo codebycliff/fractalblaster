08753a81345ffb85921f6791b119a7d3bb59fed7
cbraton <cbraton@ab504855-4d6b-4199-a8d8-a2e244a42e5e>
2011-03-07 06:46:38 +0000

    Deleted the wrong Family/Products folder in last commit. Re-added the right folder and removed the old. The solution should build fine again.

--- trunk/src/FractalBlaster/Products/EnterpriseProductModel.cs
+++ trunk/src/FractalBlaster/Products/EnterpriseProductModel.cs
@@ -7,50 +7,24 @@ using System.Windows.Forms;
 using System.Configuration;
 using System.Resources;
 using FractalBlaster.Core;
-using FractalBlaster.Runtime;
+using FractalBlaster.Core.Runtime;
+using FractalBlaster.Universe;
 
 namespace FractalBlaster.Products {
     
     public class EnterpriseProductModel : IProductModel {
 
-        public EnterpriseProductModel() {
-            mEngineTypes = new List<Type> () {
-                typeof(AudioEngine),
-                typeof(VideoEngine),
-                typeof(PhotoEngine)
-            };
-        }
-
         public String Name {
             get { return "Enterprise";  }
-        }
-
-        public  IEnumerable<Type> EngineTypes {
-            get { return mEngineTypes; } 
         }
 
         public IContainerControl DefaultUI {
             get { return new ContainerControl(); }
         }
 
-        public Boolean AllowsCustomEngines {
-            get { return true; }
+        public IEnumerable<string> SupportedFileExtensions {
+            get { return new String[] { "*" }; }
         }
-
-        public Boolean AddCustomEngine(Type engine) {
-            if (AllowsCustomEngines) {
-                mEngineTypes.Add(engine);
-                return true;
-            }
-            return false;
-        }
-
-        #region [ Private ]
-
-        private List<Type> mEngineTypes;
-        
-        #endregion
-
     }
 
 }
