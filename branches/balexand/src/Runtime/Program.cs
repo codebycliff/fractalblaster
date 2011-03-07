e007a785ea93418b41eff303eb324b07230d567f
balexander <balexander@ab504855-4d6b-4199-a8d8-a2e244a42e5e>
2011-03-07 00:49:10 +0000

    Started a new branch modeled off of Architecture documents

---
+++ branches/balexand/src/Runtime/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+//using FractalBlaster.Core.Runtime;
+using FractalBlaster.Universe;
+using FractalBlaster.PlaybackControlForm;
+//using FractalBlaster.Core;
+//using log4net.Config;
+
+namespace FractalBlaster.Runtime {
+
+    static class Program {
+        
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(String[] args) {
+
+            //BasicConfigurator.Configure();
+            //FamilyRuntime.FamilyKernel.Log = log4net.LogManager.GetLogger(typeof(Program));
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PlaybackControlForm.PlaybackControlForm());
+        }
+    }
+}
