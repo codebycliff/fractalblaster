dab6fb810adb15a923650a5c69cd30f274c088bb
balexander <balexander@ab504855-4d6b-4199-a8d8-a2e244a42e5e>
2011-03-24 20:11:35 +0000

    Added log file to debug functionality

--- branches/balexand/src/Common/Debug.cs
+++ branches/balexand/src/Common/Debug.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace FractalBlaster.Universe
 {
     static public class Debug
     {
         static DebugForm myForm;
-
         static Debug()
         {
             myForm = new DebugForm();
@@ -18,12 +18,17 @@ namespace FractalBlaster.Universe
         static public void print(string s)
         {
             myForm.print(s);
+            TextWriter logOutput = new StreamWriter("debugLog.txt", true);
+            logOutput.Write(s);
+            logOutput.Close();
         }
 
         static public void printline(string s)
         {
             myForm.printline(s);
+            TextWriter logOutput = new StreamWriter("debugLog.txt", true);
+            logOutput.WriteLine(s);
+            logOutput.Close();
         }
-
     }
 }
