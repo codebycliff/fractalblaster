e5da31efa7cd75757f4e6a3506ead6b8e9a25b5e
balexander <balexander@ab504855-4d6b-4199-a8d8-a2e244a42e5e>
2011-04-05 16:12:29 +0000

    removing src

--- trunk/src/Wave Processing/Output/Callbacks.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALLBACKS_H_
-#define CALLBACKS_H_
-
-#include "Windows.h"
-
-void CALLBACK waveOutProc(HWAVEOUT hwo, UINT uMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2);
-unsigned int CallbackBlockCompleted();
-
-#endif
