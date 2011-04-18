d38393160a259011b27f01b0a0c72901c5d1c800
ghanes <ghanes@ab504855-4d6b-4199-a8d8-a2e244a42e5e>
2011-04-18 00:37:09 +0000

    Woops, didn't add a plugin.

---
+++ trunk/src/FractalBlaster.Plugins.AboutPlugin/AboutForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FractalBlaster.Plugins.AboutPlugin
+{
+    public partial class AboutForm : Form
+    {
+        public AboutForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            //If we're shutting down windows...
+            if (e.CloseReason == CloseReason.WindowsShutDown) return;
+
+            //Else, just hide it.
+            this.Hide();
+            e.Cancel = true;
+        }
+    }
+}
---
+++ trunk/src/FractalBlaster.Plugins.AboutPlugin/AboutPlugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FractalBlaster.Universe;
+
+namespace FractalBlaster.Plugins.AboutPlugin
+{
+    [PluginAttribute(Name = "About", Author = "Fractal Blasters", Description = "About us")]
+    public class AboutPlugin : IViewPlugin
+    {
+        AboutForm form;
+
+        public AboutPlugin()
+        {
+            form = new AboutForm();
+        }
+
+        public System.Windows.Forms.Form UserInterface
+        {
+            get { return form; }
+        }
+
+        public void Initialize(AppContext context)
+        {
+        }
+    }
+}
