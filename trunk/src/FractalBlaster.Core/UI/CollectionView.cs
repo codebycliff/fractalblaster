abf0680077333d83e9209739abe61510171ef68f
klang <klang@ab504855-4d6b-4199-a8d8-a2e244a42e5e>
2011-04-19 11:51:51 +0000

    Same update as before, I reverted then committed again because I wasn't thinking straight.

--- trunk/src/FractalBlaster.Core/UI/CollectionView.cs
+++ trunk/src/FractalBlaster.Core/UI/CollectionView.cs
@@ -70,6 +70,7 @@ namespace FractalBlaster.Core.UI {
             if (HasConfiguration) {
                 mConfigureButton.Click += (sender, args) => {
                     Form form = ConfigurationDialog;
+
                     form.FormClosed += (sender2, args2) =>
                         {
                             this.RefreshItems(sender2, args2);
